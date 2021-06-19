a4dc0d74a659d62b036b278eeb6701c4e71128ac
Jonathon Marolf <jmarolf@microsoft.com>
2021-06-18 18:26:39 -0700

    wip adding dotnet-format

---
+++ src/Cli/dotnet/commands/dotnet-cleanup/CleanupForwardingApp.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.Cli.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Microsoft.DotNet.Cli
+{
+    public class CleanupForwardingApp : ForwardingApp
+    {
+        private const string FormatDllName = @"Format/dotnet-format.dll";
+
+        public CleanupForwardingApp(IEnumerable<string> argsToForward)
+            : base(Path.Combine(AppContext.BaseDirectory, FormatDllName), argsToForward)
+        {
+        }
+    }
+}
